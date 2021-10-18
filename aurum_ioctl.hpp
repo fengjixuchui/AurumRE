508732c393948f7a137df85d3dfbb577c39f431f
Kento Oki <hrn832@protonmail.com>
2021-10-18 16:27:20 +0900

    Update aurum_ioctl.hpp

--- aurum_ioctl.hpp
+++ aurum_ioctl.hpp
@@ -2,4 +2,19 @@
 #include <winioctl.h>
 
 #define AURUM_IOCTL_TYPE 0x8001
-#define AURUM_IOCTL_REGISTER_PROCESS CTL_CODE(AURUM_IOCTL_TYPE, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define AURUM_IOCTL_EXECUTE_OPERATION CTL_CODE(0x8001, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS) // 0x80012004
+
+typedef enum _AURUM_IOCTL_OPERATIONS
+{
+    AurumIoctlRegisterProtectedProcess = 0x23f931ac,
+    AurumIoctlNotYetAnalyzed1 = 0x7f1943eb,
+    AurumIoctlNotYetAnalyzed2 = 0x97F750B1,
+    AurumIoctlNotYetAnalyzed3 = 0xE1A008B5,
+} AURUM_IOCTL_OPERATIONS;
+
+typedef struct _AURUM_IOCTL_PAYLOAD
+{
+    AURUM_IOCTL_OPERATIONS operation;
+    void* context;
+    // TODO: Analyze ioctl payloads
+} AURUM_IOCTL_PAYLOAD, * PAURUM_IOCTL_PAYLOAD;
