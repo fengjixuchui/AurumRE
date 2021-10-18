945303b464ed1e092973e0c572ee5e055cd42fab
Kento Oki <hrn832@protonmail.com>
2021-10-18 15:43:27 +0900

    Update AurumPreObPseudocode.c

--- AurumPreObPseudocode.c
+++ AurumPreObPseudocode.c
@@ -833,7 +833,7 @@ LAB_140018af8:
   }
 LAB_14000e4e7:
   pcVar7 = (char *)IoGetCurrentProcess();
-  if (probable_process == pcVar7) /* Ignore current process, this value would have a pointer of ntoskrnl.exe(4) */
+  if (probable_process == pcVar7) /* Ignore an request from protected process itself*/
     goto code_r0x000140013d59;
   image_name = (ushort *)0x0;
   uVar8 = IoGetCurrentProcess();
